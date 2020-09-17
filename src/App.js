7c908ce5cb2986d07ce3f3dba7176b257bc627a2
endyjtwn <endyjtwn@gmail.com>
2020-09-18 00:29:16 +0200

    fix empty path

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import {
 } from "react-router-dom";
 
 function App() {
+
+  if (window.location.pathname === '/') window.location.href = "https://www.endyjtwn.com/about";
+
   return (
     <div className="App">
       <Router>
